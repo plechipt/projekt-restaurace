2b5ba7eb466fce7d7f1de0757e9ba1d9c2ec06ce
CrazyCat <loxtox74@gmail.com>
2022-05-25 09:34:22 +0200

    Submenu

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function toggleMenu() {
     x.style.display = "block";
   }
 }
+
+// Show submenu in hamburger link
+const links = document.querySelectorAll(".submenu-link");
+links.forEach(link => {
+  link.addEventListener('click', handleClick => {
+    const submenus = document.querySelectorAll(".hidden-menu-submenu")
+
+    submenus.forEach(submenu => {
+      console.log(submenu.style.display)
+      if (submenu.style.display === "flex") {
+        submenu.style.display = "none"
+      }
+      else {
+        submenu.style.display = "flex"
+      }
+    })
+  })
+})
