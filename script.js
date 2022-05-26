1cdeb88410ef746f93eced98d87a1a64219f4c41
Jakub Plecháč <loxtox74@gmail.com>
2022-05-26 18:50:06 +0200

    Rozbalit menu pomocí šipky

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ function toggleMenu() {
 }
 
 // Show submenu in hamburger link
-const links = document.querySelectorAll(".submenu-link");
+const links = document.querySelectorAll(".icon-right-arrow");
+console.log(links);
 links.forEach((link) => {
   link.addEventListener("click", (handleClick) => {
-    const submenu = link.querySelector("div");
+    const submenu = link.parentElement.querySelector("div");
 
     if (submenu.className === "hidden-menu-submenu") {
+      link.className = "icon-down-arrow";
       submenu.className = "shown-menu-submenu";
     } else {
+      link.className = "icon-right-arrow";
       submenu.className = "hidden-menu-submenu";
     }
   });
